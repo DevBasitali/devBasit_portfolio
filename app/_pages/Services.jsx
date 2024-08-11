f6c7e275fb8e3ae62aa4409a33ded8200799d381
DevBasitali <rajaalix2022@gmail.com>
2024-08-11 18:43:32 +0500

    its 6:43 PM now

--- app/_pages/Services.jsx
+++ app/_pages/Services.jsx
@@ -6,7 +6,7 @@ function Services() {
     <>
       <section className="relative bg-gray-900 pt-8 pb-12">
         <div className="relative flex flex-col items-center h-screen sm:h-max md:h-screen lg:h-max mx-auto max-w-screen-xl px-4 sm:px-6">
-          <div className=" max-w-xl py-6 text-center ltr:sm:text-left rtl:sm:text-right">
+          <div className=" max-w-xl py-6 text-center">
             <h1 className="text-xl font-extrabold text-white sm:text-xl md:text-4xl lg:text-5xl pb-5">Services.</h1>
           </div>
           <Card/>
@@ -17,4 +17,3 @@ function Services() {
 }
 
 export default Services;
-// sm:h-screen md:h-screen lg:flex lg:h-screen lg:px-8 bordersm:h-screen md:h-screen lg:flex lg:h-screen lg:px-8 border
--- app/_pages/Skill.jsx
+++ app/_pages/Skill.jsx
@@ -4,22 +4,43 @@ import Card from "../_components/Card";
 function Skill() {
   return (
     <>
-      {/* <section className="relative bg-gray-950 bg-cover bg-center bg-no-repeat pt-10">
-        <div className="flex items-center flex-col h-screen relative mx-auto max-w-screen-xl px-4 sm:px-6 lg:flex lg:h-screen lg:px-8">
-          <div className=" max-w-xl py-6 text-center ltr:sm:text-left rtl:sm:text-right">
-            <h1 className="text-3xl font-extrabold sm:text-5xl">Skills.</h1>
-          </div>
-        </div>
-      </section> */}
-
-      <section className="relative bg-gray-950 pt-8 pb-8">
-        <div className="relative flex flex-col items-center h-screen sm:h-max md:h-screen mx-auto max-w-screen-xl px-4 sm:px-6">
-          <div className=" max-w-xl py-6 text-center ltr:sm:text-left rtl:sm:text-right">
+      <section className="relative flex flex-row bg-gray-950">
+        <div className="relative items-center mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:px-8">
+          <div className="max-w-xl text-center hidden lg:block md:block">
             <h1 className="text-xl font-extrabold text-white sm:text-xl md:text-4xl lg:text-5xl">
               Skills.
             </h1>
+            <p className="text-sm mt-4 ml-5 max-w-lg text-gray-300 text-left justify-center   sm:text:md md:text-xl lg:text-xl tracking-wide">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              eum nam a omnis, quae eos, illum quis doloribus iste rerum nostrum
+              unde quibusdam sed molestiae consequatur voluptas autem incidunt
+              aliquam?
+            </p>
           </div>
-          
+        </div>
+
+        <div className=" flex items-center justify-center mx-auto max-w-screen-xl rounded-full bg-gray-900 my-10">
+          <div className="grid grid-cols-2 gap-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-1 mx-auto max-w-screen-xl">
+          <img src="/HTML.png" alt="html" className="w-40 h-30" />
+          <img src="/CSS.png" alt="html" className="w-40 h-30" />
+          <img src="/JAVASCRIPT.png" alt="html" className="w-40 h-30" />
+          <img src="/tailwind.png" alt="html" className="w-40 h-30" />
+            
+          </div>
+
+          <div className="grid grid-cols-2 gap-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-1">
+          <img src="/MONGODB.png" alt="html" className="w-40 h-30" />
+          <img src="/react.png" alt="html" className="w-40 h-30" />
+          <img src="/NODEJS.png" alt="html" className="w-40 h-30" />
+          <img src="/nextjs.png" alt="html" className="w-40 h-30" />
+          </div>
+
+          <div className="grid grid-cols-2 gap-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-1">
+          <img src="/github.png" alt="html" className="w-40 h-30" />
+          <img src="/Mysql.png" alt="html" className="w-40 h-30" />
+          <img src="/flutter.png" alt="html" className="w-40 h-30" />
+          <img src="/php.png" alt="html" className="w-40 h-30" />
+          </div>
         </div>
       </section>
     </>
